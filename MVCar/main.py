ccb931605eea88928596f2ccee848488efac7b37
Thomas Honnemyr <thomaseho@uia.no>
2021-10-06 10:13:18 +0200

    Rewrote the code to a class, added notification functionality

--- MVCar/main.py
+++ MVCar/main.py
@@ -3,76 +3,116 @@ import csv
 import numpy as np
 import cv2
 import pyzbar.pyzbar as pyzbar
+import smtplib, ssl
 
 
-def qr_camera():
-    cap = cv2.VideoCapture(0)
-    read_code = True
-    collected_data = []
+class QRScanner:
 
-    while True:
-        _, frame = cap.read()
+    def __init__(self, inventory_items, database_file, camera):
+        self.inv_items = self.collect_inventory_items(inventory_items)
+        self.db = database_file
+        self.cam = camera
+        self.read_codes = True
+        self.read_items = []
+        self.c_data = []
+        self.file_data = []
 
-        if read_code:
-            decodeQR = pyzbar.decode(frame)
-            if decodeQR: # Stop if it finds a QR code
-                read_code = False   # Read QR code only once
-                for obj in decodeQR:
-                    print("Data", obj.data) # Print whatever QR code it found
-                    collected_data.append(obj.data)
+    def collect_inventory_items(self, inventory_items):
 
-        cv2.imshow("Cam", frame)
-        key = cv2.waitKey(1)
-        if key == 27:
-            return collected_data
-        if key == ord("k"): # While driving it should not read codes.
-            read_code = False
+        current_items = []
 
-        if key == ord("s"): # If stopping, read QR code
-            read_code = True
+        with open(f'{inventory_items}', 'r') as inv_items:
+
+            reader = csv.reader(inv_items)
+
+            for row in reader:
+                current_items.append(row)
+
+        inv_items.close()
+        print(current_items)
+        return current_items
+
+    def scan_qr_codes(self):
+
+        while True:
+            _, frame = self.cam.read()
+
+            if self.read_codes:
+                read_item = pyzbar.decode(frame)
+                if read_item:
+                    self.read_codes = False
+                    for item in read_item:
+                        print("Read item: ", item.data)
+                        self.read_items.append(item.data)
+
+            cv2.imshow("Cam", frame)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+            if key == ord('k'):
+                self.read_codes = False
+            if key == ord('s'):
+                self.read_codes = True
+
+    def clean_data(self):
+
+        for i in range(len(self.read_items)):
+            if self.read_items[i] in self.c_data:
+                for j in range(len(self.c_data) - 1):
+                    if self.read_items[i] == self.c_data[j]:
+                        self.c_data[j + 1] += 1
+
+            else:
+                for k in range(len(self.inv_items)):
+                    if f"{self.read_items[i]}" in self.inv_items[k]:
+                        self.c_data.append(self.read_items[i])
+                        self.c_data.append(1)
+
+        for i in range(0, len(self.c_data) - 1, 2):
+            self.file_data.append([self.c_data[i], self.c_data[i + 1]])
 
 
-def clean_data(data):
+    def write_data(self):
 
-    c_data = []
-    f_data = []
+        with open(f'{self.db}', 'w') as db:
+            fieldnames = ['id', 'amount']
+            writer = csv.DictWriter(db, fieldnames=fieldnames)
+            writer.writeheader()
 
-    for i in range(len(data)):
-        if data[i] in c_data:
-            for j in range(len(c_data) - 1):
-                if data[i] == c_data[j]:
-                    c_data[j + 1] += 1
-        else:
-            c_data.append(data[i])
-            c_data.append(1)
-    for i in range(0, len(c_data) - 1, 2):
-        a = [c_data[i], c_data[i + 1]]
-        f_data.append(a)
+            for row in self.file_data:
+                writer.writerow({'id': row[0], 'amount': row[1]})
 
-    return f_data
+        db.close()
 
+    def notify_low_amount(self):
+        # Starting on email, if using Gmail emails and servers,
+        port = 587
+        password = input("Email password, then press enter:  ")
+        smtp_server = "smtp.gmail.com"
+        sender_email = "JetsonBotGroup6@gmail.com"  # Make a email for Jetson Bot, password = Passord1.
+        receiver_email = "Group6Stock@gmail.com"  # Make a test email for our "accountant"
+        message = """\
+            Subject: Stock Order
+            // Add .csv file
+            // Add "Warning low amount"
+            // Add date
+            """
 
-def write_data(data):
-
-    with open('database.csv', 'w') as database:
-        fieldnames = ['id', 'amount']
-        writer = csv.DictWriter(database, fieldnames=fieldnames)
-
-        writer.writeheader()
-
-        for row in data:
-            writer.writerow({'id': row[0], 'amount': row[1]})
-
-
-def notify_low_amount(item, amount):
-    # Future notification by email or something
-    pass
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.ehlo()  # Can be omitted
+            server.starttls(context=context)
+            server.ehlo()  # Can be omitted
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
 
 
 def main():
-    inventory_count = qr_camera()
-    result = clean_data(inventory_count)
-    write_data(result)
+
+    scanner = QRScanner('data.csv', 'database.csv', cv2.VideoCapture(0))
+    scanner.scan_qr_codes()
+    scanner.clean_data()
+    scanner.write_data()
 
 
 if __name__ == '__main__':
